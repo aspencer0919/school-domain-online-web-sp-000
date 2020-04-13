84d47451bab0ba8bec6f3985c5f20bd36d3b3112
Adam Spencer <aspencer0919@gmail.com>
2020-04-13 22:39:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,30 @@
 class School
 
-  def initialze(school_name)
+  def initialize(school_name)
     @name = name
     @roster = {}
   end
-
+  
   attr_reader :name, :roster
-
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+ def sort()
+    return_hash = {}
+    @roster.each do |key,value|
+      return_hash[key] = value.sort
+    end
+    return_hash
+end
+  
+  def add_student(name,grade)
+    if @roster.include?(grade)
+      @roster[grade]<<name
+    else
+      @roster[grade] = []
+      @roster[grade]<<name
+    end
+  end
 end
